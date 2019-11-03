a431cd7d3fa8daae454f7a5de49586bafc0848b5
Saurabh Baranwal <45738332+saurabhb12@users.noreply.github.com>
2019-11-03 23:23:11 +0530

    Create Separate_chaining.cpp

---
+++ Data_structures_implementation/Hashing/Separate_chaining.cpp
@@ -0,0 +1,85 @@
+//Question: Take input no. of lists you want in hash table. Then take input number of insertions you want to make, and hence the numbers
+//you want to insert. Then, take input number of deletions you want to do and hence the numbers you want to delete. Print the whole hash
+//table after insertion and after deletion.
+
+
+#include <bits/stdc++.h> 
+using namespace std; 
+const int N= 4e6;
+
+struct bucket
+{
+    set <int> a;
+};
+
+struct bucket table[N];
+int buckets;
+
+int hashfunction(int x)
+{
+    return x%buckets;
+}
+
+void insert(int x)
+{
+    int index= hashfunction(x);
+    table[index].a.insert(x);
+}
+
+void deleteitem(int x)
+{
+    int index= hashfunction(x);
+    int f=0;
+    for(auto i: table[index].a)
+    {
+        if(i==x)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f)
+    {
+        table[index].a.erase(x);
+    }
+}
+
+void display_hashtable()
+{
+    for(int i=0; i<buckets; i++)
+    {
+        cout<<"Bucket " <<i <<" :";
+        for(auto ass: table[i].a)
+        {
+            cout<< ass <<"| ";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main()
+{
+    cin>>buckets;
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>> x;
+        insert(x);
+    }
+    cout<<"After insertion: " <<endl;
+    display_hashtable();
+    int h;
+    cin>> h;
+    while(h--)
+    {
+        int x;
+        cin>>x;
+        deleteitem(x);
+    }
+    cout<< "After deletion: " <<endl;
+    display_hashtable();
+}
+
