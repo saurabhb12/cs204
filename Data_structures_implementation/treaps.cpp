3347d62cbf8c3a9ea89696a85b49c4a23f16d004
Saurabh Baranwal <45738332+saurabhb12@users.noreply.github.com>
2019-11-04 12:51:04 +0530

    Update treaps.cpp

--- Data_structures_implementation/treaps.cpp
+++ Data_structures_implementation/treaps.cpp
@@ -1,187 +1,208 @@
-// C++ program to demosntrate search, insert and delete in Treap 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-// A Treap Node 
-struct TreapNode 
-{ 
-    int key, priority; 
-    TreapNode *left, *right; 
-}; 
+// A Treap Node
+struct TreapNode
+{
+	int data;
+	int priority;
+	TreapNode* left, *right;
 
-TreapNode *rightRotate(TreapNode *y) 
-{ 
-    TreapNode *x = y->left, *T2 = x->right; 
+	// Constructor
+	TreapNode(int data)
+	{
+		this->data = data;
+		this->priority = rand() % 100;
+		this->left = this->right = nullptr;
+	}
+};
 
-    // Perform rotation 
-    x->right = y; 
-    y->left = T2; 
+void rotateLeft(TreapNode* &root)
+{
+	TreapNode* R = root->right;
+	TreapNode* X = root->right->left;
 
-    // Return new root 
-    return x; 
-} 
+	// rotate
+	R->left = root;
+	root->right = X;
 
-
-TreapNode *leftRotate(TreapNode *x) 
-{ 
-    TreapNode *y = x->right, *T2 = y->left; 
-
-    // Perform rotation 
-    y->left = x; 
-    x->right = T2; 
-
-    // Return new root 
-    return y; 
-} 
-
-TreapNode* newNode(int key) 
-{ 
-    TreapNode* temp = new TreapNode; 
-    temp->key = key; 
-    temp->priority = rand()%100; 
-    temp->left = temp->right = NULL; 
-    return temp; 
-} 
-
-
-TreapNode* search(TreapNode* root, int key) 
-{ 
-    
-    if (root == NULL || root->key == key) 
-    return root; 
-
-    
-    if (root->key < key) 
-    return search(root->right, key); 
-
-    return search(root->left, key); 
-} 
-
-
-TreapNode* insert(TreapNode* root, int key) 
-{ 
-    // If root is NULL, create a new node and return it 
-    if (!root) 
-        return newNode(key); 
-
-    // If key is smaller than root 
-    if (key <= root->key) 
-    { 
-        
-        root->left = insert(root->left, key); 
-
-        
-        if (root->left->priority > root->priority) 
-            root = rightRotate(root); 
-    } 
-    else 
-    { 
-        
-        root->right = insert(root->right, key); 
-
-        
-        if (root->right->priority > root->priority) 
-            root = leftRotate(root); 
-    } 
-    return root; 
-} 
-
-
-TreapNode* deleteNode(TreapNode* root, int key) 
-{ 
-    if (root == NULL) 
-        return root; 
-
-    if (key < root->key) 
-        root->left = deleteNode(root->left, key); 
-    else if (key > root->key) 
-        root->right = deleteNode(root->right, key); 
-
-    
-
-    
-    else if (root->left == NULL) 
-    { 
-        TreapNode *temp = root->right; 
-        delete(root); 
-        root = temp;
-    } 
-
-    
-    else if (root->right == NULL) 
-    { 
-        TreapNode *temp = root->left; 
-        delete(root); 
-        root = temp; 
-    } 
-
-    
-    else if (root->left->priority < root->right->priority) 
-    { 
-        root = leftRotate(root); 
-        root->left = deleteNode(root->left, key); 
-    } 
-    else
-    { 
-        root = rightRotate(root); 
-        root->right = deleteNode(root->right, key); 
-    } 
-
-    return root; 
-} 
-
-void inorder(TreapNode* root) 
-{ 
-    if (root) 
-    { 
-        inorder(root->left); 
-        cout << "key: "<< root->key << " | priority: %d "
-            << root->priority; 
-        if (root->left) 
-            cout << " | left child: " << root->left->key; 
-        if (root->right) 
-            cout << " | right child: " << root->right->key; 
-        cout << endl; 
-        inorder(root->right); 
-    } 
-} 
+	// set new root
+	root = R;
+}
 
 
 
-int main() 
-{ 
-    srand(time(NULL)); 
+void rotateRight(TreapNode* &root)
+{
+	TreapNode* L = root->left;
+	TreapNode* Y = root->left->right;
 
-    struct TreapNode *root = NULL; 
-    root = insert(root, 50); 
-    root = insert(root, 30); 
-    root = insert(root, 20); 
-    root = insert(root, 40); 
-    root = insert(root, 70); 
-    root = insert(root, 60); 
-    root = insert(root, 80); 
+	// rotate
+	L->right = root;
+	root->left = Y;
 
-    cout << "Inorder traversal of the given tree \n"; 
-    inorder(root); 
+	// set new root
+	root = L;
+}
 
-    cout << "\nDelete 20\n"; 
-    root = deleteNode(root, 20); 
-    cout << "Inorder traversal of the modified tree \n"; 
-    inorder(root); 
 
-    cout << "\nDelete 30\n"; 
-    root = deleteNode(root, 30); 
-    cout << "Inorder traversal of the modified tree \n"; 
-    inorder(root); 
+void insertNode(TreapNode* &root, int data)
+{
+	// base case
+	if (root == nullptr)
+	{
+		root = new TreapNode(data);
+		return;
+	}
 
-    cout << "\nDelete 50\n"; 
-    root = deleteNode(root, 50); 
-    cout << "Inorder traversal of the modified tree \n"; 
-    inorder(root); 
+	if (data < root->data)
+	{
+		insertNode(root->left, data);
 
-    TreapNode *res = search(root, 50); 
-    (res == NULL)? cout << "\n50 Not Found ": 
-                cout << "\n50 found"; 
+	
+		if (root->left != nullptr && root->left->priority > root->priority)
+			rotateRight(root);
+	}
+	else
+	{
+		insertNode(root->right, data);
 
-    return 0; 
-} 
+		
+		if (root->right != nullptr && root->right->priority > root->priority)
+			rotateLeft(root);
+	}
+}
+
+bool searchNode(TreapNode* root, int key)
+{
+	// if key is not present in the key
+	if (root == nullptr)
+		return false;
+
+	// if key is found
+	if (root->data == key)
+		return true;
+
+	// if given key is less than the root node, search in the left subtree
+	if (key < root->data)
+		return searchNode(root->left, key);
+
+	// else search in the right subtree
+	return searchNode(root->right, key);
+}
+
+void deleteNode(TreapNode* &root, int key)
+{
+	
+	if (root == nullptr)
+		return;
+
+	
+	if (key < root->data)
+		deleteNode(root->left, key);
+
+
+	else if (key > root->data)
+		deleteNode(root->right, key);
+
+
+	else
+	{
+
+		if (root->left == nullptr && root->right == nullptr)
+		{
+			delete root;
+			root = nullptr;
+		}
+
+		// Case 2: node to be deleted has two children
+		else if (root->left && root->right)
+		{
+			// if left child has less priority than right child
+			if (root->left->priority < root->right->priority)
+			{
+				// call rotateLeft on root
+				rotateLeft(root);
+
+				// recursively delete the left child
+				deleteNode(root->left, key);
+			}
+			else
+			{
+				// call rotateRight on root
+				rotateRight(root);
+
+				// recursively delete the right child
+				deleteNode(root->right, key);
+			}
+		}
+
+		// Case 3: node to be deleted has only one child
+		else
+		{
+			// find child node
+			TreapNode* child = (root->left)? root->left: root->right;
+			TreapNode* curr = root;
+
+			root = child;
+
+			// deallocate the memory
+			delete curr;
+		}
+	}
+}
+void printTreap(TreapNode *root, int space = 0, int height = 10)
+{
+	// Base case
+	if (root == nullptr)
+		return;
+
+	// increase distance between levels
+	space += height;
+
+	// print right child first
+	printTreap(root->right, space);
+	cout << endl;
+
+	// print current node after padding with spaces
+	for (int i = height; i < space; i++)
+		cout << ' ';
+	cout << root->data << "(" << root->priority << ")\n";
+
+	// print left child
+	cout << endl;
+	printTreap(root->left, space);
+}
+
+// main function
+int main()
+{
+	// Treap keys
+	int keys[] = { 5, 2, 1, 4, 9, 8, 10 };
+
+	int n = sizeof(keys)/sizeof(int);
+
+	// Construct a Treap
+	TreapNode* root = nullptr;
+	for (int key: keys)
+		insertNode(root, key);
+
+	cout << "Constructed Treap:\n\n";
+	printTreap(root);
+
+	cout << "\nDeleting node 1:\n\n";
+	deleteNode(root, 1);
+	printTreap(root);
+
+	cout << "\nDeleting node 5:\n\n";
+	deleteNode(root, 5);
+	printTreap(root);
+
+	cout << "\nDeleting node 9:\n\n";
+	deleteNode(root, 9);
+	printTreap(root);
+
+	return 0;
+}
