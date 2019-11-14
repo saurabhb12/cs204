dfee924e2fd775ec9c6944973e1ef01554864db1
Saurabh Baranwal <45738332+saurabhb12@users.noreply.github.com>
2019-11-14 14:14:15 +0530

    Create queue.cpp

---
+++ Data_structures_implementation/SLOT2/queue.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long 
+struct node{
+    lli data;
+    node *next;
+};
+class Queue{
+    public:
+    void enqueue(lli x)
+    {
+        node *t=(node *)malloc(sizeof(node));
+        t->data=x;
+        t->next=NULL;
+        if(this->start==NULL)
+        {
+            this->end=t;
+            this->start=t;
+        }
+        else{
+        this->end->next=t;
+        this->end=t;
+        }
+        cout<<"Added "<<x<<endl;
+    }
+    lli dequeue(void)
+    {
+        node *t=this->start;
+        if(!t)
+        {
+            cout<<"Queue is empty!!"<<endl;
+            return 0;
+        }
+        this->start=t->next;
+        lli x=t->data;
+        delete t;
+        cout<<x<<" Deleted"<<endl;
+        return x;
+    }
+    void show()
+    {
+        node *t=this->start;
+        while(t!=NULL)
+        {
+            cout<<t->data<<' ';
+            t=t->next;
+        }    
+        cout<<endl;
+    }
+    Queue()
+    {
+        this->end=NULL;
+        this->start=NULL;
+    }
+    private:
+    node *start,*end;
+};
+
+int main()
+{
+    Queue Q;
+    while(1){
+    cout<<"Enter choice : \n 1. Enqueue \n 2. Dequeue \n 3. Show \n 4. Exit \n";
+    lli x;
+    lli c;
+    cin>>c;
+    switch(c)
+    {
+        case 1: cin>>x;
+                Q.enqueue(x);
+                break;
+        case 2: Q.dequeue();
+                break;
+        case 3: Q.show();
+                break;
+        case 4: return 0;
+        default: cout<<"Wrong choice!!"<<endl;
+
+    }
+    }
+    return 0;
+}
